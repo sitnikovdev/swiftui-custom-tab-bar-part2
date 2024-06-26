57f6efa34bc6240401bc18c9898f4863330aba2e
Algene Pulido <algene.pulido@gmail.com>
2024-06-26 08:40:31 +0800

    Initial Commit

---
+++ CustomTabBarWithCenterButton/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomTabBarWithCenterButton
+//
+//  Created by Algene Pulido on 6/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomTabBarWithCenterButton/CustomTabBarWithCenterButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarWithCenterButtonApp.swift
+//  CustomTabBarWithCenterButton
+//
+//  Created by Algene Pulido on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarWithCenterButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
